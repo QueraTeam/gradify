09378d00272c8988575a6626762a4f3b44d282b3
Mohammad Javad Naderi <mjnaderi@gmail.com>
2017-04-10 22:31:05 +0430

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='gradify',
-    version='0.2',
+    version='0.3',
     py_modules=['gradify'],
     license='MIT',
     description='A python library to generate CSS gradient from an image',
