66eed07d0acdd2e062309f258ba18cbde6b46abe
Mohammad Javad Naderi <mjnaderi@gmail.com>
2017-04-10 15:03:00 +0430

    update

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import find_packages, setup
+from setuptools import setup
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='gradify',
     version='0.2',
-    packages=find_packages(),
+    py_modules=['magic'],
     license='MIT',
     description='A python library to generate CSS gradient from an image',
     long_description=README,
